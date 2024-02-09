c4ca118b5d2f70cad37eb2fc0061b0a597aae952
Zareeb <ZareebLorenzana@Gmail.Com>
2024-02-08 18:29:08 -0800

    init

---
+++ src/project-draft-s02_20240208.sql
@@ -0,0 +1,194 @@
+-- MariaDB dump 10.19  Distrib 10.5.22-MariaDB, for Linux (x86_64)
+--
+-- Host: classmysql.engr.oregonstate.edu    Database: cs340_lorenzaz
+-- ------------------------------------------------------
+-- Server version	10.6.16-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Followers`
+--
+
+DROP TABLE IF EXISTS `Followers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Followers` (
+  `followeeID` int(11) NOT NULL AUTO_INCREMENT,
+  `followerID` int(11) NOT NULL,
+  `followedSince` date NOT NULL,
+  PRIMARY KEY (`followeeID`,`followerID`),
+  KEY `followingUser_idx` (`followerID`),
+  CONSTRAINT `followedByUser` FOREIGN KEY (`followeeID`) REFERENCES `Users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `followingUser` FOREIGN KEY (`followerID`) REFERENCES `Users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Followers`
+--
+
+LOCK TABLES `Followers` WRITE;
+/*!40000 ALTER TABLE `Followers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Followers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Posts`
+--
+
+DROP TABLE IF EXISTS `Posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Posts` (
+  `postID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `postsBody` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`postID`,`userID`),
+  KEY `postedByID_idx` (`userID`),
+  CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Posts`
+--
+
+LOCK TABLES `Posts` WRITE;
+/*!40000 ALTER TABLE `Posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PostsHasLikes`
+--
+
+DROP TABLE IF EXISTS `PostsHasLikes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PostsHasLikes` (
+  `likeID` int(11) NOT NULL AUTO_INCREMENT,
+  `postID` int(11) NOT NULL,
+  `likedByUserID` int(11) NOT NULL,
+  `dateLiked` datetime NOT NULL,
+  PRIMARY KEY (`likeID`,`postID`,`likedByUserID`),
+  UNIQUE KEY `likeID_UNIQUE` (`likeID`),
+  KEY `userID_idx` (`likedByUserID`),
+  KEY `fk_postID` (`postID`),
+  CONSTRAINT `fk_postID` FOREIGN KEY (`postID`) REFERENCES `Posts` (`postID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `likedByUserID` FOREIGN KEY (`likedByUserID`) REFERENCES `Users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PostsHasLikes`
+--
+
+LOCK TABLES `PostsHasLikes` WRITE;
+/*!40000 ALTER TABLE `PostsHasLikes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PostsHasLikes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PostsHasTags`
+--
+
+DROP TABLE IF EXISTS `PostsHasTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PostsHasTags` (
+  `postID` int(11) NOT NULL,
+  `tagID` int(11) NOT NULL,
+  `dateTagged` date NOT NULL,
+  PRIMARY KEY (`postID`,`tagID`),
+  KEY `tagID_idx` (`tagID`),
+  CONSTRAINT `postID` FOREIGN KEY (`postID`) REFERENCES `Posts` (`postID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `tagID` FOREIGN KEY (`tagID`) REFERENCES `Tags` (`tagID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PostsHasTags`
+--
+
+LOCK TABLES `PostsHasTags` WRITE;
+/*!40000 ALTER TABLE `PostsHasTags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PostsHasTags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tags`
+--
+
+DROP TABLE IF EXISTS `Tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tags` (
+  `tagID` int(11) NOT NULL AUTO_INCREMENT,
+  `dateTagged` datetime NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`tagID`),
+  UNIQUE KEY `tagID_UNIQUE` (`tagID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tags`
+--
+
+LOCK TABLES `Tags` WRITE;
+/*!40000 ALTER TABLE `Tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phoneNumber` varchar(15) NOT NULL,
+  `signupDate` datetime NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `phoneNumber_UNIQUE` (`phoneNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-08 14:15:01
