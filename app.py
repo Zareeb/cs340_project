e1a54844ebb3cb0fcf98868ec6e674c98dd33836
Zareeb <ZareebLorenzana@Gmail.Com>
2024-03-02 07:02:53 -0800

    First build. CRUD enabled on Users table

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def root():
 def users():
 
     if request.method == "GET":
-        query = "SELECT * from Users"
+        query = "SELECT * FROM Users"
 
         cur = mysql.connection.cursor()
         cur.execute(query)
@@ -50,7 +50,15 @@ def users():
         phoneNumber = request.form["phoneNumber"]
         signupDate = request.form["signupDate"]
 
-        query = "INSERT INTO Users (username, firstName, lastName, email, phoneNumber, signupDate) VALUES (%s, %s, %s, %s, %s, %s)"
+        query = """INSERT INTO Users (
+                username, 
+                firstName, 
+                lastName, 
+                email, 
+                phoneNumber, 
+                signupDate) 
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """
 
         cur = mysql.connection.cursor()
 
@@ -62,9 +70,80 @@ def users():
     
     return redirect("/users")
 
+@app.route('/delete_users/<int:userID>')
+def delete_users(userID: int):
+
+    query = "DELETE FROM Users WHERE userID = %s"
+
+    cur = mysql.connection.cursor()
+
+    cur.execute(query, (userID,))
+
+    mysql.connection.commit()
+
+    return redirect('/users')
+
+@app.route('/users_edit/<int:userID>', methods=["POST", "GET"])
+def users_edit(userID: int):
+    if request.method == "GET":
+        query = "SELECT * FROM Users WHERE userID = %s" % (userID)
+        
+        cur = mysql.connection.cursor()
+
+        cur.execute(query)
+
+        data = cur.fetchall()
+
+        return render_template("users_edit.jinja2", data=data)
+        # return json.dumps(data)
+        
+    if request.method == "POST":
+        userID = request.form["userID"]
+        username = request.form["username"]
+        firstName = request.form["firstName"]
+        lastName = request.form["lastName"]
+        email = request.form["email"]
+        phoneNumber = request.form["phoneNumber"]
+        signupDate = request.form["signupDate"]
+
+        query = """UPDATE Users SET 
+                username = %s,
+                firstName = %s,
+                lastName = %s,
+                email = %s,
+                phoneNumber = %s,
+                signupDate = %s
+                WHERE userID = %s                
+                """
+        
+        cur = mysql.connection.cursor()
+
+        values = (username, firstName, lastName, email, phoneNumber, signupDate, userID)
+
+        cur.execute(query, values)
+
+        mysql.connection.commit()
+
+    return redirect('/users')
+
+@app.route('/posts')
+def posts():
+    return render_template("posts.jinja2")
+
+@app.route('/followers')
+def followers():
+    return render_template("followers.jinja2")
+
+@app.route('/likes')
+def likes():
+    return render_template("likes.jinja2")
+
+@app.route('/tags')
+def tags():
+    return render_template("tags.jinja2")
 
 # Listener
 
 if __name__ == "__main__":
     
-    app.run(port=3793, debug=True)
+    app.run(port=3792, debug=True)
