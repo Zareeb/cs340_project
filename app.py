b831169596b8b9e62e4be1f9b646cbaaf3f5e741
Zareeb <ZareebLorenzana@Gmail.Com>
2024-03-08 02:57:55 -0800

    Modularized code by using Blueprints

--- app.py
+++ app.py
@@ -7,23 +7,26 @@ Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
 
 """
 
-from flask import Flask, render_template, json, redirect
+from flask import Flask, render_template
 from flask_mysqldb import MySQL
-from flask import request
-import os
+from credentials import *
+from blueprints.users_blueprint import users_page
+from blueprints.posts_blueprint import posts_page
 
 app = Flask(__name__)
 
-# Configuration
+mysql = MySQL(app)
 
 # database connection info
-app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_lorenzaz"
-app.config["MYSQL_PASSWORD"] = "t3k1r7"
-app.config["MYSQL_DB"] = "cs340_lorenzaz"
+app.config["MYSQL_HOST"] = MYSQL_HOST
+app.config["MYSQL_USER"] = MYSQL_USER
+app.config["MYSQL_PASSWORD"] = MYSQL_PASSWORD
+app.config["MYSQL_DB"] = MYSQL_DB
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-mysql = MySQL(app)
+# Register blueprints
+app.register_blueprint(users_page, url_prefix='/users')
+app.register_blueprint(posts_page, url_prefix='/posts')
 
 # Routes 
 
@@ -31,124 +34,154 @@ mysql = MySQL(app)
 def root():
     return render_template("main.jinja2", page_title = "Home")
 
-@app.route('/users', methods=["POST", "GET"])
-def users():
+# @app.route('/users', methods=["POST", "GET"])
+# def users():   
+
+#     if request.method == "GET":
+#         query = "SELECT * FROM users"
+
+#         cur = mysql.connection.cursor()
+#         cur.execute(query)
+#         data = cur.fetchall()
+
+#         return render_template("users.jinja2", users=data, page_title = "users")
     
+#     elif request.method == "POST":
+#         username = request.form["username"]
+#         firstName = request.form["firstName"]
+#         lastName = request.form["lastName"]
+#         email = request.form["email"]
+#         phoneNumber = request.form["phoneNumber"]
+#         signupDate = request.form["signupDate"]
 
-    if request.method == "GET":
-        query = "SELECT * FROM Users"
+#         query = """INSERT INTO users (
+#                 username, 
+#                 firstName, 
+#                 lastName, 
+#                 email, 
+#                 phoneNumber, 
+#                 signupDate) 
+#                 VALUES (%s, %s, %s, %s, %s, %s)
+#                 """
 
-        cur = mysql.connection.cursor()
-        cur.execute(query)
-        data = cur.fetchall()
+#         cur = mysql.connection.cursor()
 
-        return render_template("users.jinja2", Users=data, page_title = "Users")
+#         values = (username, firstName, lastName, email, phoneNumber, signupDate)
+
+#         cur.execute(query, values)
+
+#         mysql.connection.commit()
     
-    elif request.method == "POST":
-        username = request.form["username"]
-        firstName = request.form["firstName"]
-        lastName = request.form["lastName"]
-        email = request.form["email"]
-        phoneNumber = request.form["phoneNumber"]
-        signupDate = request.form["signupDate"]
+#     return redirect("/users")
 
-        query = """INSERT INTO Users (
-                username, 
-                firstName, 
-                lastName, 
-                email, 
-                phoneNumber, 
-                signupDate) 
-                VALUES (%s, %s, %s, %s, %s, %s)
-                """
+# @app.route('/delete_users/<int:userID>')
+# def users_delete(userID: int):
 
-        cur = mysql.connection.cursor()
+#     query = "DELETE FROM users WHERE userID = %s"
 
-        values = (username, firstName, lastName, email, phoneNumber, signupDate)
+#     cur = mysql.connection.cursor()
 
-        cur.execute(query, values)
+#     cur.execute(query, (userID,))
 
-        mysql.connection.commit()
-    
-    return redirect("/users")
+#     mysql.connection.commit()
 
-@app.route('/delete_users/<int:userID>')
-def delete_users(userID: int):
+#     return redirect('/users')
 
-    query = "DELETE FROM Users WHERE userID = %s"
+# @app.route('/users_edit/<int:userID>', methods=["POST", "GET"])
+# def users_edit(userID: int):
 
-    cur = mysql.connection.cursor()
-
-    cur.execute(query, (userID,))
-
-    mysql.connection.commit()
-
-    return redirect('/users')
-
-@app.route('/users_edit/<int:userID>', methods=["POST", "GET"])
-def users_edit(userID: int):
-
-    if request.method == "GET":
-        query = "SELECT * FROM Users WHERE userID = %s" % (userID)
+#     if request.method == "GET":
+#         query = "SELECT * FROM users WHERE userID = %s" % (userID)
         
-        cur = mysql.connection.cursor()
+#         cur = mysql.connection.cursor()
 
-        cur.execute(query)
+#         cur.execute(query)
 
-        data = cur.fetchall()
+#         data = cur.fetchall()
 
-        return render_template("users_edit.jinja2", data=data, page_title = "Edit Users")
+#         return render_template("users_edit.jinja2", data=data, page_title = "Edit users")
 
-    if request.method == "POST":
-        userID = request.form["userID"]
-        username = request.form["username"]
-        firstName = request.form["firstName"]
-        lastName = request.form["lastName"]
-        email = request.form["email"]
-        phoneNumber = request.form["phoneNumber"]
-        signupDate = request.form["signupDate"]
+#     if request.method == "POST":
+#         userID = request.form["userID"]
+#         username = request.form["username"]
+#         firstName = request.form["firstName"]
+#         lastName = request.form["lastName"]
+#         email = request.form["email"]
+#         phoneNumber = request.form["phoneNumber"]
+#         signupDate = request.form["signupDate"]
 
-        query = """UPDATE Users SET 
-                username = %s,
-                firstName = %s,
-                lastName = %s,
-                email = %s,
-                phoneNumber = %s,
-                signupDate = %s
-                WHERE userID = %s                
-                """
+#         query = """UPDATE users SET 
+#                 username = %s,
+#                 firstName = %s,
+#                 lastName = %s,
+#                 email = %s,
+#                 phoneNumber = %s,
+#                 signupDate = %s
+#                 WHERE userID = %s                
+#                 """
         
-        cur = mysql.connection.cursor()
+#         cur = mysql.connection.cursor()
 
-        values = (username, firstName, lastName, email, phoneNumber, signupDate, userID)
+#         values = (username, firstName, lastName, email, phoneNumber, signupDate, userID)
 
-        cur.execute(query, values)
+#         cur.execute(query, values)
 
-        mysql.connection.commit()
+#         mysql.connection.commit()
 
-    return redirect('/users')
+#     return redirect('/users')
 
-@app.route('/posts')
-def posts():    
+# @app.route('/posts', methods=["POST", "GET"])
+# def posts():
+#     if request.method == "GET":
+#         query = "SELECT * FROM posts"
 
-    return render_template("posts.jinja2", page_title="Posts")
+#         cur = mysql.connection.cursor()
+#         cur.execute(query)
+#         data = cur.fetchall()
 
-@app.route('/followers')
-def followers():    
+#         return render_template("posts.jinja2", posts=data, page_title = "posts")
+    
+#     elif request.method == "POST":
+#         userID = request.form["userID"]
+#         postDate = request.form["postDate"]
+#         postBody = request.form["postBody"]
 
-    return render_template("followers.jinja2", page_title="Followers")
+#         query = """INSERT INTO posts (
+#                 userID, 
+#                 postDate, 
+#                 postBody) 
+#                 VALUES (%s, %s, %s)
+#                 """
 
-@app.route('/likes')
-def likes():    
+#         cur = mysql.connection.cursor()
 
-    return render_template("likes.jinja2", page_title="Likes")
+#         values = (userID, postDate, postBody)
 
-@app.route('/tags')
-def tags():    
+#         cur.execute(query, values)
 
-    return render_template("tags.jinja2", page_title="Tags")
+#         mysql.connection.commit()
+    
+#     return redirect("/posts")
+
+
+# @app.route('/followers')
+# def followers():    
+
+#     return render_template("followers.jinja2", page_title="followers")
+
+# @app.route('/likes')
+# def likes():    
+
+#     return render_template("likes.jinja2", page_title="likes")
+
+# @app.route('/tags')
+# def tags():    
+
+#     return render_template("tags.jinja2", page_title="tags")
 
 # Listener
+
+
 
 if __name__ == "__main__":
     
