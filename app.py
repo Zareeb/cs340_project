6aa28b200bca03dcf3e060083710e2fa428aa83a
Zareeb <ZareebLorenzana@Gmail.Com>
2024-03-09 07:32:23 -0800

    Implement new route to reset database

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
 
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_mysqldb import MySQL
+import subprocess
+import os
 from credentials import *
+
+# Import blueprints
 from blueprints.users_blueprint import users_page
 from blueprints.posts_blueprint import posts_page
 from blueprints.followers_blueprint import followers_page
@@ -35,6 +39,17 @@ app.register_blueprint(followers_page, url_prefix='/followers')
 @app.route('/')
 def root():
     return render_template("main.jinja2", page_title = "Home")
+
+@app.route('/reset')
+def reset_database():
+    # Get working directory
+    get_wd = os.getcwd()
+    
+    mysql_string = f"mysql -u {MYSQL_USER} -h {MYSQL_HOST} --password={MYSQL_PASSWORD} {MYSQL_DB} -e \"source {get_wd}/src/db/sql/DDL.sql\""
+
+    subprocess.run(mysql_string, shell=True)
+
+    return redirect(request.referrer)
 
 if __name__ == "__main__":
     
