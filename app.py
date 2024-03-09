e870eb0b0d2b3bb6efa6725e4a2a28635f0943a2
Zareeb <ZareebLorenzana@Gmail.Com>
2024-03-09 01:02:56 -0800

    Included register blueprint for followers

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """
 
-Authors: Marina Hampton & Zareeb Lorenzana
+Authors: Marina Hampton, Zareeb Lorenzana, & Skyler Santos
 Modified from OSU Flask starter app on GitHub
-Date: 02.29.2024
+Date: 03.07.2024
 Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
 
 """
@@ -12,12 +12,13 @@ from flask_mysqldb import MySQL
 from credentials import *
 from blueprints.users_blueprint import users_page
 from blueprints.posts_blueprint import posts_page
+from blueprints.followers_blueprint import followers_page
 
 app = Flask(__name__)
 
 mysql = MySQL(app)
 
-# database connection info
+# Imported from credentials.py
 app.config["MYSQL_HOST"] = MYSQL_HOST
 app.config["MYSQL_USER"] = MYSQL_USER
 app.config["MYSQL_PASSWORD"] = MYSQL_PASSWORD
@@ -27,6 +28,7 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 # Register blueprints
 app.register_blueprint(users_page, url_prefix='/users')
 app.register_blueprint(posts_page, url_prefix='/posts')
+app.register_blueprint(followers_page, url_prefix='/followers')
 
 # Routes 
 
@@ -36,4 +38,4 @@ def root():
 
 if __name__ == "__main__":
     
-    app.run(port=3793, debug=True)
+    app.run(port=3792, debug=True)
