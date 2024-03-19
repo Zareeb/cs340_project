84748d002c2f811f85f95ea5edc388de68bc17b5
Zareeb <ZareebLorenzana@Gmail.Com>
2024-03-18 22:56:42 -0700

    Registered new blueprints

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Modified from OSU Flask starter app on GitHub
 Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
 
 """
+# Imports required files
 from imports import *
 from credentials import *
 
@@ -15,6 +16,7 @@ from blueprints.users_blueprint import users_page
 from blueprints.posts_blueprint import posts_page
 from blueprints.followers_blueprint import followers_page
 from blueprints.likes_blueprint import likes_page
+from blueprints.postsHasTags_blueprint import postsHasTags_page
 from blueprints.tags_blueprint import tags_page
 
 app = Flask(__name__)
@@ -33,13 +35,15 @@ app.register_blueprint(users_page, url_prefix='/users')
 app.register_blueprint(posts_page, url_prefix='/posts')
 app.register_blueprint(followers_page, url_prefix='/followers')
 app.register_blueprint(likes_page, url_prefix='/likes')
+app.register_blueprint(postsHasTags_page, url_prefix='/postsHasTags')
 app.register_blueprint(tags_page, url_prefix='/tags')
 
-# Routes 
+# Route to main page 
 @app.route('/')
 def root():
     return render_template("main.jinja2", page_title = "Home")
 
+# Route for restoring database from DDL.sql file
 @app.route('/reset')
 def reset_database():
     """
@@ -48,8 +52,7 @@ def reset_database():
     # Get working directory
     get_wd = os.getcwd()
     
-    # Calls mysql and executes source to reset database
-    
+    # Calls mysql and executes source to reset database    
     mysql_string = f"mysql -u {MYSQL_USER} -h {MYSQL_HOST} --password={MYSQL_PASSWORD} {MYSQL_DB} -e \"source {get_wd}/src/db/sql/DDL.sql\""
     subprocess.run(mysql_string, shell=True)
 
@@ -57,4 +60,4 @@ def reset_database():
 
 if __name__ == "__main__":
     
-    app.run(port=3794, debug=True)
+    app.run(port=3792, debug=True)
